372e32f9ae3916583ed38880c37fd8733bb9b1ea
Llenrep <lppernell1998@gmai.com>
2019-01-17 19:42:23 -0500

    Updates

---
+++ precache-manifest.2a4cf275f6fac4ab9fb20f1dd8373142.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "42cda88db067c0a6388dafc1b9cc615d",
+    "url": "/reactDBZ/static/media/GokuBG.42cda88d.jpg"
+  },
+  {
+    "revision": "1a1e36e88e0a89ab9d85db1748ac2f9f",
+    "url": "/reactDBZ/static/media/DBSbg.1a1e36e8.mp4"
+  },
+  {
+    "revision": "3a8700d64379a466d123",
+    "url": "/reactDBZ/static/js/runtime~main.3a8700d6.js"
+  },
+  {
+    "revision": "01f216f958edd068fe09",
+    "url": "/reactDBZ/static/js/main.01f216f9.chunk.js"
+  },
+  {
+    "revision": "916b373cfef80c85a28b",
+    "url": "/reactDBZ/static/js/1.916b373c.chunk.js"
+  },
+  {
+    "revision": "01f216f958edd068fe09",
+    "url": "/reactDBZ/static/css/main.f3021d28.chunk.css"
+  },
+  {
+    "revision": "4306d8fa5f4117d60469174f120dff89",
+    "url": "/reactDBZ/index.html"
+  }
+];
